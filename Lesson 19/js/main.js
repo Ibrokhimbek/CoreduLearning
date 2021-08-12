59085ff97827817b33356bfecc680fe33649b235
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-08-12 09:19:20 +0500

    Lesson 19

---
+++ Lesson 19/js/main.js
@@ -0,0 +1,48 @@
+// var x = 5;
+// x = 10;
+// var y = 3;
+// y = 5;
+// y = 7;
+// var z = (x - y) * x; // 30
+
+// let ibrohim = 50;
+// ibrohim = 30;
+// ibrohim = 20;
+
+// const sunnat = 100;
+
+// const misol = (sunnat + x - y) * z;
+
+// console.log(x) // 5
+// console.log(x+3) // 8
+// console.log(x+y) // 8
+// console.log(misol);
+
+// var x = 3,
+//   y = 4,
+//   z = 6;
+// var x, y;
+// x = 3 + 2;
+// y = x - 5;
+
+// let x, y;
+// x = 3 + 2;
+// y = x - 5;
+
+// const first_name = "Ibrohim"; // String
+// const sur_name = "Turaboev"; // String
+// const age = 20; // Number
+// const job = "Dasturchi" // String
+
+// console.log(first_name+" "+sur_name+"ning kasbi " + job + " va uning yoshi " + age+"da");
+// console.log(`${first_name} ${sur_name}ning kasbi ${job} va uning yoshi ${age}da`) // ES6
+
+// let x = 3;
+// x++; // 4
+// x++; // 5
+// let y = 4;
+// y--; // 3
+// y--; // 2
+// console.log(y % x); // 1
+// console.log(y ** 10) // 1 048 576
+// console.log(y)
