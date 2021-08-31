0a73371b46717c433605b711917ffc617245ff73
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-08-31 09:22:37 +0500

    Lesson 25

---
+++ Lesson 25/main.js
@@ -0,0 +1,15 @@
+const cars = ["BMW", "AUDI", "HUNDAI", "MERSEDES-BENZ"]
+// const cars2 = "BNW, AUDI, HUNDAI, MERSEDES-BENZ"
+// console.log(cars.toString()) //* toString() arraydagi elementlarni stringga o'zgartiradi
+// console.log(cars.join(" * ")) //* join() arrayda elementlarni stringga o'zgartiradi va orasidagi boshliqni to'ldiradi
+// const mers = cars.pop()  //* pop() metodi oxiridan arrayning elementini oladi
+// cars.push("Hello", "Salom") //* push() metodi oxiridan arrayga element qo'shadi
+// const x = cars.push("Lacetti") //* push() metodi arrayning o'zgarishdan keyingi uzuligini qaytaradi
+// cars.shift() //* shift() arrayning boshidan elementni oladi
+// cars.unshift("Tiko") //* unshift() arrayning boshiga element qo'shadi
+// cars[cars.length] = "Matiz" //* Arrayga matizni qo'shdik
+// delete cars[0]
+// cars[0] = "Spark"
+// cars.splice(1, 1, "Lemon", "Banana")
+// cars.splice(0, 1)
+console.log(cars)
