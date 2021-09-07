ab657731f82655a5624ec8d0998374b1243d6b13
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-09-07 09:41:17 +0500

    Lesson 26

---
+++ Lesson 26/js/main.js
@@ -0,0 +1,59 @@
+// let toggle = false;
+
+// function addClass() {
+//   const p = document.querySelector("#paragraf");
+
+//   if (toggle == false) {
+//     p.classList.add("text");
+//     toggle = true;
+//   } else {
+//     p.classList.remove("text");
+//     toggle = false;
+//   }
+// }
+
+// function l() {
+//     if(5==="5") {
+//         alert("Number equals string number")
+//     }else if(5===6) {
+//         alert("Number equals Number")
+//     }else{
+//         alert("Boshqa iloji qolmadi")
+//     }
+// }
+
+// l()
+
+// const array = ["Banana", "Abror", "Orange", "Apple", "Lemon"];
+// const numbers = ["21", "11", "22", "2", "3"];
+// const numbers2 = [21, 11, 22, 2, 3];
+// array.sort()
+// array.reverse()
+// numbers.sort(function (a, b) {
+//   return a - b;
+// });
+// numbers2.sort(function (a, b) {
+//   return b-a;
+// });
+// numbers2.sort(function (a, b) {
+//   return 0.5 - Math.random();
+// });
+// numbers2.sort(function(a, b){return a - b})
+// console.log(numbers2[0]);
+// console.log(numbers2[numbers2.length-1]);
+
+// const cars = [
+//   { type: "Volvo", year: 2016 },
+//   { type: "Saab", year: 2001 },
+//   { type: "Bmw", year: 2010 },
+// ];
+
+// cars.sort(function(a, b){return a.year - b.year});
+// cars.sort(function(a, b){
+//     let x = a.type.toLowerCase();
+//     let y = b.type.toLowerCase();
+//     if (x < y) {return -1;}
+//     if (x > y) {return 1;}
+//     return 0;
+//   })
+// console.log(cars[2].type.toLowerCase())
